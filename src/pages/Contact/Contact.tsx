5c7b67f08332419fdcfe2531399e4e14fbe484ad
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-11-13 13:45:24 +0100

    DSL-59 - Refactor Contact page component and add image

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import {StyledContact, StyledH4} from './Contact.styled';
+import {StyledContact, StyledH4, StyledMan, ContactItem} from './Contact.styled';
+import man from '../../assets/man.png';
 
 export const Contact: React.FC = () => {
     return (
         <StyledContact>
-            <StyledH4>Contact Customer Support:</StyledH4>
-            <p>fintech@challenge.com</p>
-            <StyledH4>Talk to us:</StyledH4>
-            <p>(+44) 7911-123-456</p>
+            <StyledMan src={man} alt="Calculator" />
+            <ContactItem>
+                <StyledH4>Contact Customer Support:</StyledH4>
+                <p>fintech@challenge.com</p>
+                <StyledH4>Talk to us:</StyledH4>
+                <p>(+44) 7911-123-456</p>
+            </ContactItem>
         </StyledContact>
     );
 };
