2006f65242703b8e2c25ec063bed550a288e3081
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-11-14 18:20:11 +0100

    DS-68 - Added  test for invest form

--- src/components/LoansGrid/InvestForm/InvestForm.helpers.ts
+++ src/components/LoansGrid/InvestForm/InvestForm.helpers.ts
@@ -6,11 +6,11 @@ export const validationSchema = yup.object({
         .typeError('Value must be a number')
         .required('Field is required')
         .moreThan(0, 'Amount must be 0 or higher')
-        .max(1000000, 'You can select max 1 000 000'),
+        .max(10000, 'You can select max 10 000'),
     investRate: yup
         .number()
         .typeError('Value must be a number')
         .required('Field is required')
-        .max(20, 'You can select max 100%')
+        .max(20, 'You can select max 20%')
         .moreThan(0, 'Amount must be 0 or higher'),
 });
--- test/fixtures/createOfferFactory.ts
+++
@@ -1,9 +0,0 @@
-import * as Factory from 'factory.ts';
-import {CreateOfferFormValues} from '../../src/helpers/types';
-
-export const createOfferFactory = Factory.Sync.makeFactory<CreateOfferFormValues>({
-    loanAmount: 500,
-    timePeriod: 12,
-    interestRate: 7.5,
-    endDate: new Date(),
-});
