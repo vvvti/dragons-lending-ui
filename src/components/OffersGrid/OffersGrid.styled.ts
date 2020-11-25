f016fcc5dca54d957ff0278056b78a359ab8bfd3
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-11-25 10:05:07 +0100

    DSL-105 changed grid size

--- src/components/OffersGrid/OffersGrid.styled.ts
+++ src/components/OffersGrid/OffersGrid.styled.ts
@@ -6,7 +6,7 @@ export const GridView = styled.div`
     width: 80%;
     display: grid;
     grid-gap: 70px;
-    padding: 120px 15px 15px 15px;
+    padding: 100px 15px 15px 15px;
     grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
     position: relative;
 `;
